6fe521f0c27463850025851ce7c4ccee8a461d90
VictorH-D-S <135677578+VictorH-D-S@users.noreply.github.com>
2023-06-07 14:39:02 -0300

    Add files via upload

---
+++ Program - Cópia .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula03Variaveis
+{
+    class Program2
+    {
+        static void Main2(string[] args)
+        {
+            /*
+            Console.WriteLine("Bem-Vindo ao PeladoBanco!");
+            //Melhor que o NuBanco
+            Console.WriteLine("O banco mais seguro pelas pessoas que NÃO usam roupas. \n \n");
+            Console.WriteLine("Serviços Disponíveis:");
+            Console.WriteLine("- Acessar o Internet Baking");
+            Console.WriteLine("- Abrir uma Conta");
+            //Mamãe dormiu fumaça Subiu
+            Console.WriteLine("\n \n Para reclamações disque o SAC: 0800 4321 \n Outras regiões: 0800 9999");
+            Console.WriteLine("\n \n \n Obrigade por usar nossos serviços.");
+            */
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula03Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Como declarar uma variável
+
+            //O nome não pode ter:
+            // - Acentos;
+            // - Começar com números;
+            // - Começar com maiúsculo;
+
+            // ========================
+
+            // - Separar Palavras com _ ou Maiúsculo;
+
+            string dono_do_programa = "Miltom";
+            int idade = 19;
+           
+            Console.WriteLine("Programa criado por "+ dono_do_programa);
+            Console.WriteLine("Realmente, criado por " + dono_do_programa);
+            Console.WriteLine(dono_do_programa + " criou o programa.");
+            
+            //CONCATENAÇÃO = JUNTAR PEDAÇOS DE TEXTOS PARA JUNTAR UM SÓ APENAS!
+
+            // Este é o jeito véi cringe:
+            Console.WriteLine (dono_do_programa +" tem "+ idade +" anos \n  \n" + "Sou " + dono_do_programa + ", tenho " + idade +" anos.");
+
+            //Este aqui é o jeito novo poggers, chamado INTERPOLAÇÃO:
+
+            Console.WriteLine($"Meu nome é {dono_do_programa} e eu tenho {idade} anos");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
